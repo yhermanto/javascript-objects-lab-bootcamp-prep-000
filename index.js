79ad92447c25526bbaabe02d0673caa41974cc3d
youngky hermanto <yhermanto@gmail.com>
2017-09-29 06:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ var recipes = {
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+}
